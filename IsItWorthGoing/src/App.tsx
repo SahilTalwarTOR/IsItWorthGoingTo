db0730a31867850f598d79c3464421fa9a4123d6
SahilTalwarTOR <sahiltalwar2004@gmail.com>
2024-12-27 19:18:56 -0500

    Fixed some margin issues

--- IsItWorthGoing/src/App.tsx
+++ IsItWorthGoing/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Search from './Components/search'
+import Search from './Components/Search'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +10,9 @@ function App() {
   return (
     <div id="GETOUT" className="min-h-screen w-full bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
     <div id="pleaseSTOP" className='grid grid-cols-1 grid-rows-3 sm:grid-rows-3 sm:grid-cols-1 gap-2 place-items-center'>
-       <h1 className='font-inter text-white text-4xl sm:text-5xl md:text-[4.5rem] mt-64 md:mt-[50%] lg:mt-64 animate__animated animate__fadeInUp'>Is It Worth Going To?</h1>
+       <h1 className='font-inter text-white text-4xl sm:text-5xl md:text-[4.5rem] mt-64 md:mt-[50%] lg:mt-48 animate__animated animate__fadeInUp'>Is It Worth Going To?</h1>
        <Search></Search>
+       <h6 className='font-inter text-slate-400 text-xs md:text-md mb-64 md:mb-64 animate__animated animate__fadeIn'>Made by Sahil Talwar</h6>
     </div>
     </div>
   )
